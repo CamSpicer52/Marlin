feddccbaea6183b3a3df8b94505e4f5f67ff646b
CamSpicer52 <130315243+CamSpicer52@users.noreply.github.com>
2023-10-29 23:42:00 -0500

    Updated X steps
    https://docs.google.com/spreadsheets/d/1MVfgcRqYL24Nxt1CzIDmiWpZNMTfsSaifwDuCyCTHWA/edit#gid=1881165038

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1230,7 +1230,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 81.04, 85.30, 3211.83, 413.35 } 
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.92, 85.30, 3211.83, 413.35 } 
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
