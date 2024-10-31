b8a82273ff1ddb0d1b30eea1d0c7039245cb8e7f
CamSpicer52 <130315243+CamSpicer52@users.noreply.github.com>
2024-10-30 23:08:51 -0500

    Turn led brigthness down

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -3567,7 +3567,7 @@
   //#define NEOPIXEL2_PIN               5
   #define NEOPIXEL_PIXELS              8 // Number of LEDs in the strip. (Longest strip when NEOPIXEL2_SEPARATE is disabled.)
   #define NEOPIXEL_IS_SEQUENTIAL          // Sequential display for temperature change - LED by LED. Disable to change all LEDs at once.
-  #define NEOPIXEL_BRIGHTNESS         200 // Initial brightness (0-255)
+  #define NEOPIXEL_BRIGHTNESS         100 // Initial brightness (0-255)
   //#define NEOPIXEL_STARTUP_TEST         // Cycle through colors at startup
 
   // Support for second Adafruit NeoPixel LED driver controlled with M150 S1 ...
@@ -3601,7 +3601,7 @@
  *  - Turn off after the print has finished and the user has pushed a button
  */
 #if ANY(BLINKM, RGB_LED, RGBW_LED, PCA9632, PCA9533, NEOPIXEL_LED)
-  #define PRINTER_EVENT_LEDS
+  //#define PRINTER_EVENT_LEDS
 #endif
 
 // @section servos
